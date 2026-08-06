ebba5d8523708c4319d8b2164d753068b8fb538a
prashantsharma-svg <prashantsharma@ipsacademy.org>
2026-08-06 09:44:27 +0530

    hello1

--- abc.cpp
+++ abc.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
+    for (int i = 1; i <= 12; i++) {
+        for (int j = 1; j <= 12; j++) {
             cout << i * j << "\t";
         }
         cout << endl;
